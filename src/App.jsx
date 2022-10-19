ec6047d6a8cd227d9a5232c0a2a1667b2c271aa2
sazzad <sazzadkarim01@gmail.com>
2022-10-19 17:38:34 +0600

    project initialize successfully

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-black w-full overflow-hidden">
+      <div className="app">
+        <div className="">Navbar</div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
